a2241296af1853a323c82955050b4b707bfca72b
Sahand Atlasi <sahand.atlasi@studio.unibo.it>
2026-09-19 17:58:20 +0200

    feat: basic kivey application

--- meetnote/__main__.py
+++ meetnote/__main__.py
@@ -1,3 +1,16 @@
-import meetnote
+from kivy.app import App
+from kivy.uix.label import Label
 
-meetnote.main()
+
+class MeetNoteApp(App):
+    title = "MeetNote"
+
+    def build(self) -> Label:
+        return Label(
+            text="MeetNote is running!",
+            font_size="28sp",
+        )
+
+
+if __name__ == "__main__":
+    MeetNoteApp().run()
